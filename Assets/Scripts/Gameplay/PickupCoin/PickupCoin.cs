8769a15b3a128f22945b594c97ec08dbdff7d20f
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2017-12-01 05:42:39 +0200

    added Coroutine usage example

--- Assets/Scripts/Gameplay/PickupCoin/PickupCoin.cs
+++ Assets/Scripts/Gameplay/PickupCoin/PickupCoin.cs
@@ -13,6 +13,10 @@ public class PickupCoin : MonoBehaviour
 	{
 	}
 
+	private void OnDestroy()
+	{
+	}
+
 	void Update()
 	{
 		transform.rotation *= Quaternion.Euler(new Vector3(0, rotateSpeed * Mathf.Sin(Time.time), 0));
@@ -23,8 +27,37 @@ public class PickupCoin : MonoBehaviour
 		UserResources.CollectCoin();
 		GetComponent<MeshRenderer>().enabled = false;
 		GetComponent<SphereCollider>().enabled = false;
-		particles.Play();
 		AudioManager.PlaySFX(AudioResources.Instance.collect_coin);
-		Destroy(gameObject, particles.main.duration);
+
+		//Destroy(gameObject, particles.main.duration);
+
+		particles.Play();
+		particles.transform.SetParent(transform.parent);
+
+		// Starts a couroutine using the GameManager MonoBehaviour
+		Coroutine coroutine = GameManager.Get().StartCoroutine(DestroyParticle(particles.main.duration));
+
+		// Destroy the pickup object
+		Destroy(gameObject);
 	}
+
+	private IEnumerator ShowMessage(float delay)
+	{
+		int frames = 0;
+		while (frames < 500)
+		{
+			yield return new WaitForEndOfFrame();
+			frames++;
+		}
+
+		Debug.Log("Frames: " + frames);
+	}
+
+	private IEnumerator DestroyParticle(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		Debug.Log("Destroy particle");
+		Destroy(particles.gameObject);
+	}
+
 }
