4d941e670d6eda61aa3ddbe83d49d0a28c2e0879
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2017-12-01 05:31:19 +0200

    added animator example (Gate object)

---
+++ Assets/Scripts/Gameplay/Gate/GateAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateAnimation : MonoBehaviour
+{
+	private Animator animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animator = GetComponent<Animator>();
+		animator.speed = 3;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other.tag);
+		if (other.tag == "Player")
+		{
+			animator.SetBool("isOpen", true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+
+		if (other.tag == "Player")
+		{
+			animator.SetBool("isOpen", false);
+		}
+	}
+
+}
